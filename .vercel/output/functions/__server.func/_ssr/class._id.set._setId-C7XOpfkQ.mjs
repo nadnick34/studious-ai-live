c11d7af0d7867650db2cee0d88ebee34201a17b8
Daniel Nickerson <daniel.nickerson@gmail.com>
2026-08-21 00:09:45 -0500

    Remove Vercel output folder

--- .vercel/output/functions/__server.func/_ssr/class._id.set._setId-C7XOpfkQ.mjs
+++
@@ -1,6 +0,0 @@
-import { C as require_jsx_runtime, h as Outlet } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/class._id.set._setId-C7XOpfkQ.js
-var import_jsx_runtime = require_jsx_runtime();
-var SplitComponent = () => /* @__PURE__ */ (0, import_jsx_runtime.jsx)(Outlet, {});
-//#endregion
-export { SplitComponent as component };
